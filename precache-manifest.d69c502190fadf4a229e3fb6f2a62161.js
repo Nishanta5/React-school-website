fd679d44bd423946f779fffda9a26edfe1ff6ab6
Nishanta5 <nishanta.dutta55@gmail.com>
2024-07-31 13:38:08 +0530

    Updates

---
+++ precache-manifest.d69c502190fadf4a229e3fb6f2a62161.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08adb92f9626fea131c812127217f27",
+    "url": "/React-school-website/index.html"
+  },
+  {
+    "revision": "3e412eafa31001b43d6f",
+    "url": "/React-school-website/static/css/main.8097afab.chunk.css"
+  },
+  {
+    "revision": "750e22a0e5088f0d31c9",
+    "url": "/React-school-website/static/js/2.ce559e35.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/React-school-website/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "3e412eafa31001b43d6f",
+    "url": "/React-school-website/static/js/main.f289dff4.chunk.js"
+  },
+  {
+    "revision": "83e69b7fa314ba9c650e",
+    "url": "/React-school-website/static/js/runtime~main.3128c966.js"
+  },
+  {
+    "revision": "518997435cafbdb52146404e0439e5b0",
+    "url": "/React-school-website/static/media/Classroom.51899743.jpg"
+  },
+  {
+    "revision": "90f8bb8bce1afd4669fbf4d31e382227",
+    "url": "/React-school-website/static/media/Library.90f8bb8b.jpg"
+  },
+  {
+    "revision": "f21eb5e6108ea1a6c6f61dafcca9c616",
+    "url": "/React-school-website/static/media/aboutus.f21eb5e6.jpg"
+  },
+  {
+    "revision": "1dbcaef999c696082d47691864a1eb0c",
+    "url": "/React-school-website/static/media/academics.1dbcaef9.jpg"
+  },
+  {
+    "revision": "8e381bbc13769a9bd65d75ee7038b11e",
+    "url": "/React-school-website/static/media/admissions.8e381bbc.jpg"
+  },
+  {
+    "revision": "69af6d5a5bc30b1cc2c48fbe203eeccd",
+    "url": "/React-school-website/static/media/btech.69af6d5a.jpg"
+  },
+  {
+    "revision": "e9701d671c72ff25536c678ffd0d926d",
+    "url": "/React-school-website/static/media/pexels-raymond-ma-yi-rong-504251-14700793.e9701d67.jpg"
+  },
+  {
+    "revision": "914095e998eb162a7bd773033ed67ded",
+    "url": "/React-school-website/static/media/pexels-shvets-production-8899940.914095e9.jpg"
+  },
+  {
+    "revision": "5ee518480582dd27c67b65dfae434402",
+    "url": "/React-school-website/static/media/pexels-tdcat-102448.5ee51848.jpg"
+  },
+  {
+    "revision": "e436e4d032f5502133f180cc0ecce7a6",
+    "url": "/React-school-website/static/media/school main.e436e4d0.jpg"
+  },
+  {
+    "revision": "d94877794b551474c7d623d0291e0d00",
+    "url": "/React-school-website/static/media/school.d9487779.jpg"
+  },
+  {
+    "revision": "539dcd1128ec7ebd63fbf22bfeb7b4d9",
+    "url": "/React-school-website/static/media/science exhibition.539dcd11.PNG"
+  },
+  {
+    "revision": "b64ba3421fff422e316cacde377386de",
+    "url": "/React-school-website/static/media/teacher2.b64ba342.jpg"
+  },
+  {
+    "revision": "9246b02163c760c7ad699d5d10c108b3",
+    "url": "/React-school-website/static/media/teacher3.9246b021.jpg"
+  },
+  {
+    "revision": "749bcf5dfa8b7cdb2abb586e4bf1c9cf",
+    "url": "/React-school-website/static/media/vice principal (1).749bcf5d.jpg"
+  }
+]);
